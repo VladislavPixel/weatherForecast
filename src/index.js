6adc1a31166efa79b1b5ec52c7724a2c9331023a
pixel <pixel.world7@mail.ru>
2022-02-24 23:05:01 +0300

    project setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./css/style.css"
+import backgroundImage from "./assets/background.jpg"
+
+
+const img = document.createElement("img")
+
+img.src = backgroundImage
+
+document.body.append(img)
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "build"),
+	},
+	devServer: {
+		contentBase: "./build",
+		port: 8080,
+		open: true
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html"
+		}),
+		new CleanWebpackPlugin(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [
+							["@babel/preset-env", { targets: "defaults" }]
+						]
+					}
+				}
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|jpe?g|gif|jpg|svg)$/i,
+				use: [
+					{
+						loader: "file-loader",
+					},
+				],
+			},
+		]
+	}
+}
